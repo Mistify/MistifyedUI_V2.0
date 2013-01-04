2bba09342da4279b7af9f3e3a2cdbe79b7ff4ca5
Mistify <mistifyed@live.com>
2013-01-04 10:03:24 -0500

    Added some stuff

--- MistifyedUI/Modules/Chat/Chat.lua
+++ MistifyedUI/Modules/Chat/Chat.lua
@@ -16,23 +16,40 @@ TukuiInfoLeft:SetPoint("LEFT", UIParent, "BOTTOMLEFT", 150, 120)
 
 
 TukuiLineToABLeft:ClearAllPoints()
-TukuiLineToABLeft:SetWidth(641)
+TukuiLineToABLeft:SetWidth(636)
 TukuiLineToABLeft:SetPoint("LEFT", UIParent, "BOTTOMLEFT", 135, 120)
 
 TukuiInfoLeftLineVertical:ClearAllPoints()
 TukuiInfoLeftLineVertical:SetHeight(10)
 TukuiInfoLeftLineVertical:SetPoint("BOTTOM", TukuiLineToABLeft, "LEFT", 0,0)
 
+local ileftlv = CreateFrame("Frame", "VertLineLeft", UIParent)
+ileftlv:SetTemplate()
+ileftlv:Size(2, 58)
+ileftlv:Point("LEFT", TukuiLineToABLeft, "RIGHT", 0, 0)
+ileftlv:SetFrameLevel(1)
+ileftlv:SetFrameStrata("BACKGROUND")
+G.Panels.BottomLeftVerticalLine = ileftlv
+
+
 -------Right Chat Area Setup-------
 -----------------------------------------
 TukuiInfoRight:SetPoint("RIGHT", UIParent, "BOTTOMRIGHT", -150, 120)
 
 TukuiLineToABRight:ClearAllPoints()
-TukuiLineToABRight:SetWidth(641)
+TukuiLineToABRight:SetWidth(635)
 TukuiLineToABRight:SetPoint("RIGHT", UIParent, "BOTTOMRIGHT", -135, 120)
 
 TukuiInfoRightLineVertical:ClearAllPoints()
 TukuiInfoRightLineVertical:SetHeight(10)
 TukuiInfoRightLineVertical:SetPoint("BOTTOM", TukuiLineToABRight, "RIGHT", 0,0)
 
+
+local irightlv = CreateFrame("Frame", "VertLineRight", UIParent)
+irightlv:SetTemplate()
+irightlv:Size(2, 58)
+irightlv:Point("RIGHT", TukuiLineToABRight, "LEFT", 0, 0)
+irightlv:SetFrameLevel(1)
+irightlv:SetFrameStrata("BACKGROUND")
+G.Panels.BottomRightVerticalLine = irightlv
 
